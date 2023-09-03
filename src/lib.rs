168ef25ac4201ad83126762b64d5f5fab138d841
Evan Jones <evan_t_jones@mac.com>
2023-09-03 16:27:55 -0500

    Made most arguments optional. Added PyColorMode enum, but using it would require extra code on the Python side

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,47 @@
 use pyo3::prelude::*;
 use std::path::PathBuf;
+use std::str::FromStr;
 use vtracer::{Config, convert_image_to_svg, ColorMode, Hierarchical };
 use visioncortex::PathSimplifyMode;
 
+#[pyclass]
+enum PyColorMode {
+    Color,
+    Binary
+}
+
+impl FromStr for PyColorMode {
+
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<PyColorMode, Self::Err> {
+        match input {
+            "binary" => Ok(PyColorMode::Binary),
+            "color"  => Ok(PyColorMode::Color),
+            _        => Ok(PyColorMode::Color),
+        }
+    }
+}
+
 #[pyfunction]
-// #[pyo3(signature = (image_path = "String", out_path = "String"))]
 fn convert_image_to_svg_py( image_path: String, 
                             out_path: String,   
-                            colormode: String,      // "color" or "binary"
-                            hierarchical: String,   // "stacked" or "cutout"
-                            filter_speckle: usize, // default: 4
-                            mode: String,           // "polygon", "spline", "none"
-                            color_precision: i32,
-                            layer_difference: i32,
-                            corner_threshold: i32,
-                            length_threshold: f64, // in [3.5, 10]
-                            splice_threshold: i32,
-                            max_iterations: usize,
+                            colormode: String,     // "color" or "binary"
+                            hierarchical: String,  // "stacked" or "cutout"
+                            mode: String,          // "polygon", "spline", "none"
+                            filter_speckle: Option<usize>, // default: 4
+                            color_precision: Option<i32>,
+                            layer_difference: Option<i32>,
+                            corner_threshold: Option<i32>,
+                            length_threshold: Option<f64>, // in [3.5, 10]
+                            max_iterations: Option<usize>,
+                            splice_threshold: Option<i32>,
                             path_precision: Option<u32>) -> PyResult<()> {
     let input_path = PathBuf::from(image_path);
     let output_path = PathBuf::from(out_path);
 
-    // TODO: enforce color mode with an enum so that we only accept the 
-    // strings 'color' or 'binary'
+    // TODO: enforce color mode with an enum so that we only 
+    // accept the strings 'color' or 'binary'
     let color_mode = match colormode.as_str() {
         "color" => ColorMode::Color,
         "binary" => ColorMode::Binary,
@@ -42,17 +61,13 @@ fn convert_image_to_svg_py( image_path: String,
         _ => PathSimplifyMode::Spline,
     };
 
-    // let color_mode = ColorMode::Binary;
-    // let filter_speckle = speckle.unwrap_or(4);
-    // let path_precision = path_precision.unwrap_or(3);
-    // let config = Config {
-    //     input_path,
-    //     output_path,
-    //     color_mode,
-    //     filter_speckle,
-    //     path_precision,
-    //     ..Default::default()
-    // };
+    let filter_speckle =    filter_speckle.unwrap_or(4);
+    let color_precision =   color_precision.unwrap_or(6);
+    let layer_difference =  layer_difference.unwrap_or(16);
+    let corner_threshold =  corner_threshold.unwrap_or(60);
+    let length_threshold =  length_threshold.unwrap_or(4.0);
+    let splice_threshold =  splice_threshold.unwrap_or(45);
+    let max_iterations =    max_iterations.unwrap_or(10);
 
     let config = Config {
         input_path,
@@ -76,18 +91,9 @@ fn convert_image_to_svg_py( image_path: String,
     Ok(())
 }
 
-/// Formats the sum of two numbers as string.
-// DEBUG: remove this function before publishing
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn vtracer_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    // DEBUG: remove this function before publishing
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(convert_image_to_svg_py, m)?)?;
     Ok(())
 }
